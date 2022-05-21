b8599fa540e1359c89fc85a77ff19691986bf92d
Ilaria Refaat <ilariarefaa20@gmail.com>
2022-05-21 12:47:29 +0200

    removing comments

--- project-hci/src/app/app.module.ts
+++ project-hci/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { NavigationBarComponent } from "./Masters/components/navigation-bar/navigation-bar.component";
+
+
 import { AuthService } from './services/auth.service';
 import { ProfileGuardService } from './services/profile-guard.service';
 
 
-=======
+
 import { NavigationBarComponent } from './Masters/components/navigation-bar/navigation-bar.component';
 import { IvyCarouselModule } from 'angular-responsive-carousel';
 import { FooterComponent } from './Masters/footer/footer.component';
->>>>>>> b393cf5e4a97ee24e88fab8dd56212da07766d92
+
 
 @NgModule({
   declarations: [
@@ -22,19 +22,20 @@ import { FooterComponent } from './Masters/footer/footer.component';
     NavigationBarComponent,
     routingComponents,
     FooterComponent,
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     IvyCarouselModule,
+
   ],
-<<<<<<< HEAD
+
+
   providers: [ProfileGuardService, AuthService],
   bootstrap: [AppComponent]
-=======
-  providers: [],
-  bootstrap: [AppComponent],
->>>>>>> b393cf5e4a97ee24e88fab8dd56212da07766d92
+
 })
-export class AppModule {}
+export class AppModule { }
