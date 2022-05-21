b393cf5e4a97ee24e88fab8dd56212da07766d92
Seif Elewa <44884545+seifelewa@users.noreply.github.com>
2022-05-21 04:00:48 +0200

    Footer_ver2

--- project-hci/src/app/app.module.ts
+++ project-hci/src/app/app.module.ts
@@ -4,23 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from "./Masters/components/navigation-bar/navigation-bar.component";
-import {IvyCarouselModule} from 'angular-responsive-carousel';
-
+import { NavigationBarComponent } from './Masters/components/navigation-bar/navigation-bar.component';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { FooterComponent } from './Masters/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    routingComponents
+    routingComponents,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    IvyCarouselModule
+    IvyCarouselModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- project-hci/src/app/pages/homepage/homepage.component.ts
+++ project-hci/src/app/pages/homepage/homepage.component.ts
@@ -4,18 +4,13 @@ import { IvyCarouselModule } from 'angular-responsive-carousel';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
-
   i = 0;
-events = [
-    {"image": "project-hci\src\assets\Carousel\maroon5.png",
-     "alt": "Description" }
-]
+  events = [
+    { image: 'project-hcisrcassetsCarouselmaroon5.png', alt: 'Description' },
+  ];
 
-  
-ngOnInit(): void {
-}
-
+  ngOnInit(): void {}
 }
